8aae7c767fc552b23ebaf89fa68c754b3e2f1620
Ananta Pratama <rinna@Anantas-MacBook-Pro.local>
2019-01-12 10:28:35 +0700

    Added article typing

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,26 @@
-interface Welcome {
-  message: string
-  status: boolean
+interface Article {
+  status: string
+  copyright: string
+  response: Response
+}
+
+interface Response {
+  docs: Doc[]
+}
+
+interface Doc {
+  web_url: string
+  snippet: string
+  source: string
+  multimedia: Multimedia[]
+  headline: Headline
+  _id: string
+}
+
+interface Headline {
+  main: string
+}
+
+interface Multimedia {
+  url: string
 }
