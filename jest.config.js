ebcd34cecef901f85f158cbbb07fa489daeb2d5c
Ananta Pratama <rinna@Anantas-MacBook-Pro.local>
2019-01-13 23:37:54 +0700

    Test init and added search bar component test

---
+++ jest.config.js
@@ -0,0 +1,24 @@
+const { defaults: tsjPreset } = require("ts-jest/presets")
+
+module.exports = {
+  ...tsjPreset,
+  preset: "react-native",
+  transform: {
+    ...tsjPreset.transform,
+    "\\.js$": "<rootDir>/node_modules/react-native/jest/preprocessor.js"
+  },
+  globals: {
+    "ts-jest": {
+      babelConfig: true
+    }
+  },
+  // This is the only part which you can keep
+  // from the above linked tutorial's config:
+  cacheDirectory: ".jest/cache",
+  setupFiles: ["./src/__tests__/setup.js"],
+  testPathIgnorePatterns: [
+    "./src/__tests__/setup.js",
+    "\\.snap$",
+    "<rootDir>/node_modules/"
+  ]
+}
